1231ec4741371a49b2765c5a807c1a3440507b5a
Devon Wijesinghe <devon@devons-macbook-pro.local>
2019-08-26 12:32:30 +0530

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Devon Wijesinghe on 8/26/19.
+//  Copyright © 2019 Devon Wijesinghe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
